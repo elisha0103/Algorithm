4539786c9481ea286952bb9e605fada0b0a33029
elisha0103 <41459466+elisha0103@users.noreply.github.com>
2022-07-04 13:38:03 +0900

    Create Croatia_alpha2.c

---
+++ Croatia_alpha2.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include<string.h>
+// 입력받은 총 문자열 개수에서 크로아티아 문자에 해당한다면 총 문자열에서 차감하는 방식
+// 메모리 관리에 도움됨.
+
+int main(void) {
+  char words[100];
+  int cnt = 0;
+  int len;
+
+  scanf("%s", words);
+  cnt = strlen(words);
+  for(int i = 0; i < strlen(words); i ++)
+    {
+      if(words[i] == '=')
+      {
+        if(words[i-1] == 's')
+        {
+          cnt--;
+        }
+        else if(words[i-1] == 'z')
+        {
+          cnt--;
+          if(words[i-2] == 'd')
+          {
+            cnt--;
+          }
+        }
+        else if(words[i-1] == 'c')
+        {
+          cnt --;
+        }
+      }
+      else if(words[i] == '-')
+      {
+        if(words[i-1] == 'c')
+        {
+          cnt --;
+        }
+        else if(words[i-1] == 'd')
+        {
+          cnt--;
+        }
+      }
+      else if(words[i] == 'j')
+      {
+        if(words[i-1] == 'l')
+        {
+          cnt--;
+        }
+        else if(words[i-1] == 'n')
+        {
+          cnt--;
+        }
+      }
+    }
+  printf("%d", cnt);
+  }
